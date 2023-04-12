51671a93d7a2e78d62efc664a579817dac997bc1
Thomas von Dein <tom@vondein.org>
2023-04-12 20:25:04 +0200

    fix ack

--- conf-lisp/grep.el
+++ conf-lisp/grep.el
@@ -1,3 +1,11 @@
 ;; https://github.com/leoliu/ack-el
-(use-package ack)
 
+(defun tvd-kill-ack()
+  ;; FIXME: still asks!
+  (local-set-key (kbd "q") 'kill-buffer-and-window))
+
+(use-package ack
+  :config
+  (setq ack-defaults-function 'ack-legacy-defaults))
+
+(add-hook 'ack-mode-hook 'tvd-kill-ack)
