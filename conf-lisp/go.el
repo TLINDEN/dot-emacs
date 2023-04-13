89459719b549038f9ce88a4601ec5ad240d3a607
Thomas von Dein <tom@vondein.org>
2023-04-13 07:23:24 +0200

    removed tab indent setting, works w/o it and doesn interfere w/ global

--- conf-lisp/go.el
+++ conf-lisp/go.el
@@ -7,7 +7,7 @@
              :config
              (setq gofmt-args '("-s"))
              (setq tab-width 4)
-             (setq indent-tabs-mode 1)
+             ;; (setq indent-tabs-mode 1)
 
              :init
              (add-hook 'before-save-hook 'gofmt-before-save))
