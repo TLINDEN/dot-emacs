ea50412bc5847a367d8974f2875bc97598cf3545
Thomas von Dein <tom@vondein.org>
2023-04-13 07:23:09 +0200

    fixed ack problems

--- TODO.md
+++
@@ -1,3 +0,0 @@
-# c-y y doesnt work
-    unknown argument for?!?!
-# ack 'q' still asks to kill process
--- conf-lisp/grep.el
+++ conf-lisp/grep.el
@@ -1,11 +1,20 @@
 ;; https://github.com/leoliu/ack-el
 
 (defun tvd-kill-ack()
+  (interactive)
+  (let ((kill-buffer-query-functions nil))
+    (delete-window)
+    (kill-buffer "*ack*")))
+
+(defun tvd-hook-kill-ack()
   ;; FIXME: still asks!
-  (local-set-key (kbd "q") 'kill-buffer-and-window))
+  (local-set-key (kbd "q") 'tvd-kill-ack))
 
 (use-package ack
   :config
-  (setq ack-defaults-function 'ack-legacy-defaults))
+  (setq ack-defaults-function 'ack-legacy-defaults)
+  (advice-add 'ack-mode :after
+    '(lambda ()
+       (switch-to-buffer "*ack*"))))
 
-(add-hook 'ack-mode-hook 'tvd-kill-ack)
+(add-hook 'ack-mode-hook 'tvd-hook-kill-ack)
