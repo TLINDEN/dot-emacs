d5ca899137e3db239ae6887634508668866b7071
Thomas von Dein <tom@vondein.org>
2023-04-24 18:59:47 +0200

    add lsp-mode (go only for now)

--- conf-lisp/go.el
+++ conf-lisp/go.el
@@ -10,4 +10,20 @@
              ;; (setq indent-tabs-mode 1)
 
              :init
-             (add-hook 'before-save-hook 'gofmt-before-save))
+             ;; disabled, I'm now trying lsp-mode, see below:
+             ;; (add-hook 'before-save-hook 'gofmt-before-save)
+             ;;   :hook (go-mode lsp-deferred)
+
+             (when (fboundp 'lsp-deferred)
+               (defun lsp-go-install-save-hooks ()
+                 (add-hook 'before-save-hook #'lsp-format-buffer t t)
+                 (add-hook 'before-save-hook #'lsp-organize-imports t t))
+
+               (add-hook 'go-mode-hook #'lsp-deferred)
+               (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+               (add-hook 'go-mode-hook #'ivy-mode)
+
+               ;; overwrite dump-jump settions here
+               (bind-key*  (kbd "C-c j") #'lsp-find-definition)
+               (bind-key*  (kbd "C-c b") #'xref-pop-marker-stack)
+               ))
